5c6193aa3b3ca29d9c902359c5fbac648cf5b134
Maurício Bruno <mb.bruno.s@gmail.com>
2023-03-07 15:29:50 -0300

    feat: adds progress status

--- src/components/GameScreen/GameScreen.tsx
+++ src/components/GameScreen/GameScreen.tsx
@@ -6,6 +6,7 @@ import { GameContext } from "../../context/GameContext";
 import { RepoCard } from "../RepoCard";
 import { useRandomRepos } from "../../hooks";
 import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
+import { BsChevronLeft, BsFillSkipEndFill } from "react-icons/bs";
 
 export const GameScreen: React.FC = () => {
   const { repos, setIsGameStarted, setIsGameFinished, score, setScore } =
@@ -14,6 +15,8 @@ export const GameScreen: React.FC = () => {
   const [repoA, setRepoA] = useState<Repo>({} as Repo);
   const [repoB, setRepoB] = useState<Repo>({} as Repo);
   const randomRepos = useRandomRepos();
+  const [counter, setCounter] = useState(1);
+  const totalMatches = Math.floor(repos.length / 2);
 
   useEffect(() => {
     updateRepos();
@@ -48,6 +51,8 @@ export const GameScreen: React.FC = () => {
     } else {
       setScore((prevScore) => ({ ...prevScore, wrong: prevScore.wrong + 1 }));
     }
+
+    setCounter((prevCounter) => prevCounter + 1);
     updateRepos();
   }
 
@@ -56,6 +61,7 @@ export const GameScreen: React.FC = () => {
   }
 
   function handleSkip() {
+    setCounter((prevCounter) => prevCounter + 1);
     updateRepos();
   }
 
@@ -69,13 +75,19 @@ export const GameScreen: React.FC = () => {
         <>
           <h1 className={styles.title}>Choose the repo with the most stars</h1>
 
-          <div className={styles.score}>
-            <p className={styles.scoreCorrect}>
-              <FaCheckCircle /> {score.correct}
-            </p>
-            <p className={styles.scoreWrong}>
-              <FaTimesCircle /> {score.wrong}
-            </p>
+          <div className={styles.gameStatus}>
+            <div className={styles.score}>
+              <p className={styles.scoreCorrect}>
+                <FaCheckCircle /> {score.correct}
+              </p>
+              <p className={styles.scoreWrong}>
+                <FaTimesCircle /> {score.wrong}
+              </p>
+            </div>
+
+            <div className={styles.counter}>
+              {counter}/{totalMatches}
+            </div>
           </div>
 
           <div className={styles.reposContainer}>
@@ -88,10 +100,10 @@ export const GameScreen: React.FC = () => {
 
       <div className={styles.buttons}>
         <button className={styles.button} onClick={handleReturnGame}>
-          Return to Menu
+          <BsChevronLeft /> Return to Menu
         </button>
         <button className={styles.button} onClick={handleSkip}>
-          Skip
+          Skip <BsFillSkipEndFill />
         </button>
       </div>
     </div>
